e17d4c054834c6a58ddeebd00ed673eda7808c3f
Bennie <phoenixshadow2456@gmail.com>
2025-04-24 20:31:14 -0500

    connecting sql to python

---
+++ project.py
@@ -0,0 +1,17 @@
+import mysql.connector
+
+connection = mysql.connector.connect(user = 'root', database = 'elite 102 project', password = 'password')
+
+cursor = connection.cursor()
+
+testQuery = ("SELECT * FROM account")
+
+cursor.execute(testQuery)
+
+for item in cursor:
+    print(item)
+
+cursor.close()
+
+connection.close()
+
